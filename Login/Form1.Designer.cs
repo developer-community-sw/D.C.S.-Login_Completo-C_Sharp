6cdb49602b4ed0a8d1adc2d53b518b4eaa3243de
jhasmany_jhunnior@outlook.com <jhasmany>
2020-02-23 07:18:32 -0400

    subiendo archivo proyecto login al repositorio

---
+++ Login/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.linkLpass = new System.Windows.Forms.LinkLabel();
+            this.picBoxminimizar = new System.Windows.Forms.PictureBox();
+            this.picBoxcerrar = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxminimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxcerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 300);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape2,
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(780, 300);
+            this.shapeContainer1.TabIndex = 1;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // lineShape1
+            // 
+            this.lineShape1.BorderColor = System.Drawing.Color.DimGray;
+            this.lineShape1.Enabled = false;
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 303;
+            this.lineShape1.X2 = 741;
+            this.lineShape1.Y1 = 91;
+            this.lineShape1.Y2 = 91;
+            // 
+            // txtuser
+            // 
+            this.txtuser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.txtuser.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtuser.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtuser.ForeColor = System.Drawing.Color.DimGray;
+            this.txtuser.Location = new System.Drawing.Point(303, 66);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(439, 20);
+            this.txtuser.TabIndex = 1;
+            this.txtuser.Text = "USUARIO";
+            this.txtuser.Enter += new System.EventHandler(this.txtuser_Enter);
+            this.txtuser.Leave += new System.EventHandler(this.txtuser_Leave);
+            // 
+            // txtpass
+            // 
+            this.txtpass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.txtpass.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtpass.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpass.ForeColor = System.Drawing.Color.DimGray;
+            this.txtpass.Location = new System.Drawing.Point(303, 144);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(439, 20);
+            this.txtpass.TabIndex = 2;
+            this.txtpass.Text = "CONTRASEÑA";
+            this.txtpass.Enter += new System.EventHandler(this.txtpass_Enter);
+            this.txtpass.Leave += new System.EventHandler(this.txtpass_Leave);
+            // 
+            // lineShape2
+            // 
+            this.lineShape2.BorderColor = System.Drawing.Color.DimGray;
+            this.lineShape2.Enabled = false;
+            this.lineShape2.Name = "lineShape2";
+            this.lineShape2.X1 = 304;
+            this.lineShape2.X2 = 742;
+            this.lineShape2.Y1 = 170;
+            this.lineShape2.Y2 = 170;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DimGray;
+            this.label1.Location = new System.Drawing.Point(475, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 33);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "LOGIN";
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnlogin.FlatAppearance.BorderSize = 0;
+            this.btnlogin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnlogin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnlogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnlogin.ForeColor = System.Drawing.Color.LightGray;
+            this.btnlogin.Location = new System.Drawing.Point(304, 208);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(408, 40);
+            this.btnlogin.TabIndex = 3;
+            this.btnlogin.Text = "ACCEDER";
+            this.btnlogin.UseVisualStyleBackColor = false;
+            // 
+            // linkLpass
+            // 
+            this.linkLpass.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.linkLpass.AutoSize = true;
+            this.linkLpass.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLpass.LinkColor = System.Drawing.Color.DimGray;
+            this.linkLpass.Location = new System.Drawing.Point(406, 271);
+            this.linkLpass.Name = "linkLpass";
+            this.linkLpass.Size = new System.Drawing.Size(190, 17);
+            this.linkLpass.TabIndex = 0;
+            this.linkLpass.TabStop = true;
+            this.linkLpass.Text = "¿ Ha olvidado contraseña ?";
+            // 
+            // picBoxminimizar
+            // 
+            this.picBoxminimizar.Image = global::Login.Properties.Resources.menos;
+            this.picBoxminimizar.Location = new System.Drawing.Point(714, 3);
+            this.picBoxminimizar.Name = "picBoxminimizar";
+            this.picBoxminimizar.Size = new System.Drawing.Size(29, 31);
+            this.picBoxminimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxminimizar.TabIndex = 8;
+            this.picBoxminimizar.TabStop = false;
+            this.picBoxminimizar.Click += new System.EventHandler(this.picBoxminimizar_Click);
+            // 
+            // picBoxcerrar
+            // 
+            this.picBoxcerrar.Image = global::Login.Properties.Resources.cerrar;
+            this.picBoxcerrar.Location = new System.Drawing.Point(748, 9);
+            this.picBoxcerrar.Name = "picBoxcerrar";
+            this.picBoxcerrar.Size = new System.Drawing.Size(20, 20);
+            this.picBoxcerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxcerrar.TabIndex = 7;
+            this.picBoxcerrar.TabStop = false;
+            this.picBoxcerrar.Click += new System.EventHandler(this.picBoxcerrar_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Login.Properties.Resources.solo_el_logo_negro_transparente;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 25);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(224, 244);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(780, 300);
+            this.Controls.Add(this.picBoxminimizar);
+            this.Controls.Add(this.picBoxcerrar);
+            this.Controls.Add(this.linkLpass);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.shapeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxminimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxcerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private System.Windows.Forms.TextBox txtuser;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape2;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.LinkLabel linkLpass;
+        private System.Windows.Forms.PictureBox picBoxcerrar;
+        private System.Windows.Forms.PictureBox picBoxminimizar;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Login/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwmd, int wmsw, int wparam, int lparam);
+
+        private void txtuser_Enter(object sender, EventArgs e)
+        {
+            if (txtuser.Text == "USUARIO") { 
+                txtuser.Text = "";
+                txtuser.ForeColor = Color.LightGray;
+            }
+        }
+
+        private void txtuser_Leave(object sender, EventArgs e)
+        {
+            if (txtuser.Text == "") {
+                txtuser.Text = "USUARIO";
+                txtuser.ForeColor = Color.DimGray;
+            }
+        }
+
+        private void txtpass_Enter(object sender, EventArgs e)
+        {
+            if (txtpass.Text == "CONTRASEÑA")
+            {
+                txtpass.Text = "";
+                txtpass.ForeColor = Color.LightGray;
+                txtpass.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtpass_Leave(object sender, EventArgs e)
+        {
+            if (txtpass.Text == "")
+            {
+                txtpass.Text = "CONTRASEÑA";
+                txtpass.ForeColor = Color.DimGray;
+                txtpass.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void picBoxcerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void picBoxminimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+    }
+}
